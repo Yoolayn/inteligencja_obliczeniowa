a96546fe81e555a6fbf1ef681fd8a24cf1125998
Yoolayn <jaroszynskijulian@gmail.com>
2024-03-06 16:35:14 +0100

    fix: zadanie 2

--- pca.py
+++ pca.py
@@ -1,41 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from sklearn import datasets
 from sklearn.decomposition import PCA
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import pandas as pd
-
 
 iris = datasets.load_iris()
-x = pd.DataFrame(iris.data, columns=iris.feature_names)
-y = pd.Series(iris.target, name="FlowerType")
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-pca_iris = PCA(n_components=3).fit(iris.data)
+num_features = X.shape[1]
 
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-iris_reduced = PCA(n_components=3).fit_transform(iris.data)
+for i in range(1, num_features):
+    pca = PCA(n_components=i)
+    X_pca = pca.fit_transform(X)
+    explained_variance_ratio = np.sum(pca.explained_variance_ratio_)
+    print(f"Liczba komponentów: {i}, wariancja: {explained_variance_ratio*100}%")
+    if explained_variance_ratio >= 0.95:
+        break
 
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(
-    iris_reduced[:, 0],
-    iris_reduced[:, 1],
-    iris_reduced[:, 2],
-    cmap=plt.cm.Paired,
-    c=iris.target
-)
-for k in range(3):
-    ax.scatter(
-        iris_reduced[y == k, 0],
-        iris_reduced[y == k, 1],
-        iris_reduced[y == k, 2],
-        label=iris.target_names[k]
+print(f"Optymalna liczba komponentów: {i}")
+
+cmap = plt.colormaps.get_cmap("viridis")
+
+if i == 2:
+    plt.figure(figsize=(8, 6))
+    scatter = plt.scatter(X_pca[:, 0], X_pca[:, 1], c=iris.target, cmap=cmap)
+    plt.title("PCA on Iris Dataset (2 components)")
+    plt.legend(
+        handles=scatter.legend_elements()[0],
+        labels=list(iris.target_names)
     )
+    plt.show()
 
-ax.set_title("First three P.C.")
-ax.set_xlabel("P.C. 1")
-ax.set_ylabel("P.C. 2")
-ax.set_zlabel("P.C. 3")
-plt.legend(numpoints=1)
-plt.show()
+elif i == 3:
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection="3d")
+    scatter = ax.scatter(
+        X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=iris.target, cmap=cmap
+    )
+    ax.set_title("PCA on Iris Dataset (3 components)")
+    legend = ax.legend(
+        handles=scatter.legend_elements()[0],
+        labels=list(iris.target_names),
+        loc="upper right",
+        title="Species",
+    )
+    ax.add_artist(legend)
+    plt.show()
