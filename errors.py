7e9b40a8ac031fc874269aeac543c331795d473c
Yoolayn <jaroszynskijulian@gmail.com>
2024-03-06 16:17:49 +0100

    fix: lower more than 15

--- errors.py
+++ errors.py
@@ -20,6 +20,13 @@ if __name__ == "__main__":
     df["petal.length"] = df["petal.length"].mask(df["petal.length"] < 0)
     df["petal.width"] = df["petal.width"].mask(df["petal.width"] < 0)
 
+    df["sepal.length"] = df["sepal.length"].mask(df["sepal.length"] > 15)
+    df["sepal.width"] = df["sepal.width"].mask(df["sepal.width"] > 15)
+    df["petal.length"] = df["petal.length"].mask(df["petal.length"] > 15)
+    df["petal.width"] = df["petal.width"].mask(df["petal.width"] > 15)
+
+    print(df.isnull().sum())
+
     median_sl = df["sepal.length"].median()
     median_sw = df["sepal.width"].median()
     median_pl = df["petal.length"].median()
@@ -36,3 +43,4 @@ if __name__ == "__main__":
     # petal.length    0  petal.length    0
     # petal.width     1  petal.width     1
     # variety         1  variety         1
+    print(df.values)
