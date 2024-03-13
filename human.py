7a1cb835a061506733c76be7baf3d8e5206f3792
Yoolayn <jaroszynskijulian@gmail.com>
2024-03-13 16:18:50 +0100

    feat: zadania

---
+++ human.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("iris.csv")
+
+(train_set, test_set) = train_test_split(
+    df.values,
+    train_size=0.7,
+    random_state=288496
+)
+
+
+print(df)
+print(test_set)
+print(test_set.shape[0])
+
+
+train_inputs = train_set[:, 0:4]
+train_classes = train_set[:, 4]
+test_inputs = test_set[:, 0:4]
+test_classes = test_set[:, 4]
+
+
+def classify_iris(sl, sw, pl, pw):
+    if pw < 1:
+        return "Setosa"
+    if sl > 6 and pw >= 1.8:
+        return "Virginica"
+    return "Versicolor"
+
+
+good_predictions = 0
+len = test_set.shape[0]
+
+for i in range(len):
+    if (
+        classify_iris(
+            sl=train_inputs[i][0],
+            sw=train_inputs[i][1],
+            pl=train_inputs[i][2],
+            pw=train_inputs[i][3],
+        )
+        == test_classes[i]
+    ):
+        good_predictions = good_predictions + 1
+        print("GOOOD")
+    print(
+        train_inputs[i][0],
+        train_inputs[i][2],
+        train_inputs[i][2],
+        train_inputs[i][3]
+    )
+    print(
+        "Guess:",
+        classify_iris(
+            sl=train_inputs[i][0],
+            sw=train_inputs[i][1],
+            pl=train_inputs[i][2],
+            pw=train_inputs[i][3],
+        ),
+    )
+    print(test_classes[i])
+
+print(good_predictions)
+print(good_predictions / len * 100, "%")
---
+++ other.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    f1_score,
+)
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+
+from sklearn.naive_bayes import GaussianNB
+
+# Load the dataset
+df = pd.read_csv("iris.csv")
+
+# Split the dataset into training and testing sets (70%/30%)
+train_set, test_set = train_test_split(df, test_size=0.3, random_state=288496)
+
+# Display the training and testing sets
+print("Training set:")
+print(train_set)
+print("\nTesting set:")
+print(test_set)
+
+# Split each set into input features and target class
+train_inputs = train_set.drop("variety", axis=1)
+train_classes = train_set["variety"]
+test_inputs = test_set.drop("variety", axis=1)
+test_classes = test_set["variety"]
+
+
+def resultComp(tool, prediction, answers):
+    accuracy = accuracy_score(prediction, answers)
+    f1 = f1_score(prediction, answers, average="weighted")
+    matrix = confusion_matrix(prediction, answers)
+    scores = cross_val_score(tool, train_inputs, train_classes, cv=5)
+
+    print(matrix)
+    print("Accuracy:", accuracy)
+    print("F1 Score:", f1)
+    print("Scores:", scores)
+    print("--------------")
+
+
+def knn(list):
+    for k in list:
+        knn = KNeighborsClassifier(n_neighbors=k, metric="euclidean")
+        print(knn)
+        knn.fit(train_inputs, train_classes)
+
+        output = knn.predict(test_inputs)
+
+        print(f"{k}NN")
+        resultComp(knn, output, test_classes)
+
+
+knn([5, 5, 11])
+
+bayes = GaussianNB()
+bayes.fit(train_inputs, train_classes)
+bayesOutput = bayes.predict(test_inputs)
+
+
+print("Bayes")
+resultComp(bayes, bayesOutput, test_classes)
